4af03f3e0048d8025c4bd2cc663789289758ec55
MarynaHuz789 <electra247@gmail.com>
2024-01-21 10:51:04 +0000

    Summary

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,16 +1,9 @@
-// створити значення, що задає кінцеву дату
-// створити значення, що задає стартову дату
-// вирахувати різницю між датами яка буде зазначена у функції зворотнього відліку
-// знайти потрібні елементи
-
-//імпорт бібліотек
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-//знайшли елементи
 const input = document.querySelector("#datetime-picker")
 const startBtn = document.querySelector('[data-start]')
 const daysElem = document.querySelector('[data-days]')
@@ -21,26 +14,19 @@ const secondsElem = document.querySelector('[data-seconds]')
 let userSelectedDate 
 
 
-// функія для додавання нулів на початок
 function addLeadingZero(value) {
     return String(value).padStart(2, "0");
 }
 
-//функія для підрахунку значень таймера
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
     return { days, hours, minutes, seconds };
@@ -59,14 +45,12 @@ function upateCountDown() {
     }
     const { days, hours, minutes, seconds } = convertMs(timeDifference)
     
-//оновлюємо інтерфейс таймера
  daysElem.textContent = addLeadingZero(days)
  hoursElem.textContent = addLeadingZero(hours)
  minutesElem.textContent = addLeadingZero(minutes)
  secondsElem.textContent = addLeadingZero(seconds)
 }
 
-//функція для запуску таймера
 function startTimer() {
     setInterval(upateCountDown, 1000)
 }
